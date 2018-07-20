568e8a3312dacdc8489c7b6ce7377da34f0c4e1e
SSE4 <tomskside@gmail.com>
2018-07-20 22:22:21 +0700

    - libm is needed on Linux
    Signed-off-by: SSE4 <tomskside@gmail.com>

--- conanfile.py
+++ conanfile.py
@@ -102,4 +102,6 @@ class OpusConan(ConanFile):
 
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
+        if self.settings.os == 'Linux':
+            self.cpp_info.libs.append('m')
         self.cpp_info.includedirs.append(os.path.join('include', 'opus'))
