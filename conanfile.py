8c8f85a73d744618050a30d8fa36dda146a39e51
Stephan Vedder <stephan.vedder@gmail.com>
2019-06-04 23:49:34 +0200

    Migrate to the CMake buildsystem and use opus 1.3.1

--- conanfile.py
+++ conanfile.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from conans import ConanFile, tools, AutoToolsBuildEnvironment, MSBuild
+from conans import ConanFile, tools, AutoToolsBuildEnvironment, MSBuild, CMake
 import os
 import shutil
 
 
 class OpusConan(ConanFile):
     name = "opus"
-    version = "1.2.1"
+    version = "1.3.1"
     url = "https://opus-codec.org/"
     description = "Opus is a totally open, royalty-free, highly versatile audio codec."
     author = "Bincrafters <bincrafters@gmail.com>"
@@ -17,7 +17,7 @@ class OpusConan(ConanFile):
     license = "BSD-3-Clause"
     homepage = "https://opus-codec.org"
     exports = ["LICENSE.md"]
-    exports_sources = ["FindOPUS.cmake"]
+    exports_sources = ["FindOPUS.cmake","opus_buildtype.cmake"]
 
     settings = "os", "arch", "compiler", "build_type"
     options = {"shared": [True, False], "fPIC": [True, False], "fixed_point": [True, False]}
@@ -34,85 +34,38 @@ class OpusConan(ConanFile):
                 (self.settings.compiler != "Visual Studio" or int(str(self.settings.compiler.version)) < 14):
             raise tools.ConanException("On Windows, the opus package can only be built with the "
                                        "Visual Studio 2015 or higher.")
-        if self.settings.compiler == "Visual Studio" and not self.options.shared:
-            self.options.remove("fixed_point")
 
     def config_options(self):
         if self.settings.os == "Windows":
             self.options.remove("fPIC")
 
+
     def source(self):
         source_url = "https://archive.mozilla.org/pub/opus"
         tools.get("{0}/{1}-{2}.tar.gz".format(source_url, self.name, self.version),
-                  sha256="cfafd339ccd9c5ef8d6ab15d7e1a412c054bf4cb4ecbbbcc78c12ef2def70732")
+                  sha256="65b58e1e25b2a114157014736a3d9dfeaad8d41be1c8179866f144a2fb44ff9d")
         extracted_dir = self.name + "-" + self.version
         os.rename(extracted_dir, self._source_subfolder)
-        # exclude opus_compare.c from build (it has main function)
-        with tools.chdir(os.path.join(self._source_subfolder, 'win32', 'VS2015')):
-            tools.replace_in_file('opus.vcxproj',
-                                  '    <ClCompile Include="..\\..\\src\\opus_compare.c">\n'
-                                  '      <DisableSpecificWarnings>4244;%(DisableSpecificWarnings)'
-                                  '</DisableSpecificWarnings>\n'
-                                  '    </ClCompile>\n', '')
+        # They forgot to package that file into the tarball for 1.3.1
+        # See https://github.com/xiph/opus/issues/129
+        os.rename("opus_buildtype.cmake", self._source_subfolder + "/opus_buildtype.cmake")
 
-    @property
-    def fixed_point(self):
-        if self.settings.compiler == 'Visual Studio' and not self.options.shared:
-            return False
-        return self.options.fixed_point
-
-    def build_msvc(self):
-        with tools.chdir(self._source_subfolder):
-            pc_build = 'fixed-point' if self.fixed_point else 'floating-point'
-            shutil.copy('opus.pc.in', 'opus.pc')
-            tools.replace_in_file('opus.pc', '@VERSION@', self.version)
-            tools.replace_in_file('opus.pc', '@PC_BUILD@', pc_build)
-            tools.replace_in_file('opus.pc', '@LIBM@', '')
-            tools.replace_in_file('opus.pc', '@prefix@', os.path.abspath(self.package_folder))
-            tools.replace_in_file('opus.pc', '@exec_prefix@', '${prefix}')
-            tools.replace_in_file('opus.pc', '@libdir@', '${exec_prefix}/lib')
-            tools.replace_in_file('opus.pc', '@includedir@', '${prefix}/include')
-        with tools.chdir(os.path.join(self._source_subfolder, "win32", "VS2015")):
-            btype = "%s%s%s" % (self.settings.build_type, "DLL" if self.options.shared else "",
-                                "_fixed" if self.fixed_point else "")
-            msbuild = MSBuild(self)
-            msbuild.build('opus.sln', build_type=btype, platforms={"x86": "Win32"})
-
-    def build_configure(self):
-        def chmod_plus_x(filename):
-            if os.name == 'posix':
-                os.chmod(filename, os.stat(filename).st_mode | 0o111)
-
-        with tools.chdir(self._source_subfolder):
-            if self.settings.os == "Macos":
-                tools.replace_in_file("configure", r"-install_name \$rpath/", "-install_name ")
-            chmod_plus_x('configure')
-            if self.options.shared:
-                args = ['--disable-static', '--enable-shared']
-            else:
-                args = ['--disable-shared', '--enable-static']
-            args.append('--enable-fixed-point' if self.fixed_point else '--disable-fixed-point')
-            env_build = AutoToolsBuildEnvironment(self)
-            env_build.configure(args=args)
-            env_build.make()
-            env_build.install()
+    def _configure_cmake(self):
+        cmake = CMake(self)
+        cmake.definitions["OPUS_FIXED_POINT"] = self.options.fixed_point
+        cmake.definitions["BUILD_SHARED_LIBS"] = self.options.shared
+        cmake.configure(source_folder=self._source_subfolder)
+        return cmake
 
     def build(self):
-        if self.settings.compiler == "Visual Studio":
-            self.build_msvc()
-        else:
-            self.build_configure()
+        cmake = self._configure_cmake()
+        cmake.build()
 
     def package(self):
         self.copy("FindOPUS.cmake", ".", ".")
         self.copy("COPYING", dst="licenses", src=self._source_subfolder, keep_path=False)
-        if self.settings.compiler == 'Visual Studio':
-            self.copy(pattern="*", dst=os.path.join("include", "opus"),
-                      src=os.path.join(self._source_subfolder, "include"), keep_path=False)
-            self.copy(pattern="*.dll", dst="bin", src=self._source_subfolder, keep_path=False)
-            self.copy(pattern="*.lib", dst="lib", src=self._source_subfolder, keep_path=False)
-            self.copy("*.pc", dst=os.path.join("lib", "pkgconfig"), src=self._source_subfolder)
-            self.copy(pattern="**.pdb", dst="bin", keep_path=False)
+        cmake = self._configure_cmake()
+        cmake.install()
 
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
