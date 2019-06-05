2b54e643bb3f6eb1d3a22ab5c7df7dd705e3c663
SSE4 <tomskside@gmail.com>
2019-06-05 10:05:18 +0200

    - remove unused imports

--- conanfile.py
+++ conanfile.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from conans import ConanFile, tools, AutoToolsBuildEnvironment, MSBuild, CMake
+from conans import ConanFile, tools, CMake
 import os
 import shutil
 
