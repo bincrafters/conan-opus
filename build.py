b1276cca6f5947bd03769158c2eb7785f3e19a84
Croydon <git@cr0ydon.com>
2019-11-03 21:23:36 +0100

    Apply conventions

--- build.py
+++ build.py
@@ -1,17 +1,9 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 
 from bincrafters import build_template_default
 
 if __name__ == "__main__":
 
-    builder = build_template_default.get_builder(pure_c=True)
-
-    items = []
-    for item in builder.items:
-        if not (item.settings["compiler"] == "Visual Studio" and item.settings["compiler.version"] == "12"):
-            items.append(item)
-    builder.items = items
+    builder = build_template_default.get_builder()
 
     builder.run()
